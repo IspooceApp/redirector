30a43a421fe668d4a309d0b70581665b6d5f79e6
yaman1337 <yaman.sarabariya@gmail.com>
2022-10-03 20:06:04 +0545

    changed port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors())
 
 app.get("/", (req, res) => {
     try {
-        res.send(`<h1>Due to large file size of ISS model, we had to change our server to another platform. Please click <a href="http://206.81.1.111:5173">here</a> to view our project. It takes some time to load.</h1>`)
+        res.send(`<h1>Due to large file size of ISS model, we had to change our server to another platform. Please click <a href="http://206.81.1.111:8080">here</a> to view our project. It takes some time to load.</h1>`)
     } catch (error) {
         console.log(error);
         res.send("Internal server error.")
